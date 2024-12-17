9663fdbd35a59a8c3694427d06e0388603131e3e
YasmineZaatour <117782662+YasmineZaatour@users.noreply.github.com>
2024-12-18 00:45:20 +0100

    test

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-console.log("Firebase API Key: ", process.env.REACT_APP_FIREBASE_API_KEY);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
